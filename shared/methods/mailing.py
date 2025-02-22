0180c99088939c34558abd78269707228b729c5f
Ivan Bersenev <bersenev4dev@gmail.com>
2025-02-22 15:35:08 +0700

    hello

--- shared/methods/mailing.py
+++ shared/methods/mailing.py
@@ -12,6 +12,31 @@ async def mailing(photo_path: str, text: str, type: str):
 
     for user in users:
         if user_validate_by_type(type, user):
+            try:
+                await clear_chat(user)
+
+                keyboard = kb.main_kb(user.player_id == -31415926)
+                hello_message = await bot.send_message(chat_id=user.telegram_id, text="Привет!", reply_markup=keyboard)
+
+                msg = await bot.send_photo(
+                    chat_id=user.telegram_id,
+                    photo=FSInputFile(photo_path),
+                    caption=text
+                )
+                user.last_message_id = msg.message_id
+                await tg_users_data.update_user(user)
+                await tg_users_data.add_to_all_messages(user, message_id=hello_message.message_id)
+                await tg_users_data.add_to_all_messages(user, message_id=msg.message_id)
+            except:
+                print('ошибка')
+
+
+async def ids_mailing(photo_path: str, text: str, ids_path: str):
+    file = open(ids_path, 'r')
+    content = file.readlines()
+    for id in content:
+        try:
+            user = await tg_users_data.get_user_by_player_id(id)
             await clear_chat(user)
 
             keyboard = kb.main_kb(user.player_id == -31415926)
@@ -26,27 +51,8 @@ async def mailing(photo_path: str, text: str, type: str):
             await tg_users_data.update_user(user)
             await tg_users_data.add_to_all_messages(user, message_id=hello_message.message_id)
             await tg_users_data.add_to_all_messages(user, message_id=msg.message_id)
-
-
-async def ids_mailing(photo_path: str, text: str, ids_path: str):
-    file = open(ids_path, 'r')
-    content = file.readlines()
-    for id in content:
-        user = await tg_users_data.get_user_by_player_id(id)
-        await clear_chat(user)
-
-        keyboard = kb.main_kb(user.player_id == -31415926)
-        hello_message = await bot.send_message(chat_id=user.telegram_id, text="Привет!", reply_markup=keyboard)
-
-        msg = await bot.send_photo(
-            chat_id=user.telegram_id,
-            photo=FSInputFile(photo_path),
-            caption=text
-        )
-        user.last_message_id = msg.message_id
-        await tg_users_data.update_user(user)
-        await tg_users_data.add_to_all_messages(user, message_id=hello_message.message_id)
-        await tg_users_data.add_to_all_messages(user, message_id=msg.message_id)
+        except:
+            print('ошибка')
 
 
 def user_validate_by_type(type: str, user: User):
--- shared/methods/send_video_notes.py
+++ shared/methods/send_video_notes.py
@@ -10,19 +10,22 @@ async def send_video_notes(video_path: str):
     users = await tg_users_data.get_all_users()
 
     for user in users:
-        await clear_chat(user)
+        try:
+            await clear_chat(user)
 
-        keyboard = kb.main_kb(user.player_id == -31415926)
-        hello_message = await bot.send_message(chat_id=user.telegram_id, text="Привет!", reply_markup=keyboard)
+            keyboard = kb.main_kb(user.player_id == -31415926)
+            hello_message = await bot.send_message(chat_id=user.telegram_id, text="Привет!", reply_markup=keyboard)
 
-        video = FSInputFile(video_path)
+            video = FSInputFile(video_path)
 
-        msg = await bot.send_video_note(
-            video_note=video,
-            chat_id=user.telegram_id
-        )
+            msg = await bot.send_video_note(
+                video_note=video,
+                chat_id=user.telegram_id
+            )
 
-        user.last_message_id = msg.message_id
-        await tg_users_data.update_user(user)
-        await tg_users_data.add_to_all_messages(user, message_id=hello_message.message_id)
-        await tg_users_data.add_to_all_messages(user, message_id=msg.message_id)
+            user.last_message_id = msg.message_id
+            await tg_users_data.update_user(user)
+            await tg_users_data.add_to_all_messages(user, message_id=hello_message.message_id)
+            await tg_users_data.add_to_all_messages(user, message_id=msg.message_id)
+        except:
+            print('error')
